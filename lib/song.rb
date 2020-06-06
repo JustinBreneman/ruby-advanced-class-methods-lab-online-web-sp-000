17154067c93d79c432d350f00b16d3161a9f38e0
Justin Breneman <justin.breneman@yahoo.com>
2020-06-06 02:52:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,10 @@ class Song
   def self.find_by_name(name)
     self.all.find{ |song| song.name == name}
   end
+
+  def self.find_or_create_by_name
+    if self.find_by_name == nil
+      self.create_by_name(name)
+    end
+  end
 end
